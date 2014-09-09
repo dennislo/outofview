a9df8ef893856fbed3f07d3a298f3cd8b26fb3ae
Dennis Lo <dennis.lo@industrieit.com>
2014-09-09 17:05:16 +0100

    first commit with boilerplate setup

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name outofviewBusStopApp
+ * @description
+ * # outofviewBusStopApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('outofviewBusStopApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngTouch'
+  ]);
